925696a5840fd75d9a788a6a5fe5a24f137cdc72
tmrsich <thomrs24@bergen.org>
2023-04-05 10:04:03 -0400

    Made changes to the script to target elements with class names instead of by id

--- public/scripts/fade-in.js
+++ public/scripts/fade-in.js
@@ -1,4 +1,11 @@
-window.addEventListener('load', () => {
-    const helloStatement = document.getElementById('hello-statement');
-    helloStatement.classList.add('fade-in');
-  });
+// Wait for the page to finish loading
+window.addEventListener("load", function() {
+  
+  // Get all elements with class "fade"
+  const elements = document.getElementsByClassName("fade");
+  
+  // Add the class "fade-in" to each element
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].classList.add("fade-in");
+  }
+});
