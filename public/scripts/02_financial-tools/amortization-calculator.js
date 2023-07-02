2777fd0b01c1f0ae6d5f63ce836bd54eb882bcaa
Thomas Mrsich <thomrs24@bergen.org>
2023-07-01 22:52:46 -0400

    Added javascript functionality

---
+++ public/scripts/02_financial-tools/amortization-calculator.js
@@ -0,0 +1,57 @@
+document.getElementById("submit").addEventListener("click", calculateAmortization);
+
+function calculateAmortization() {
+  var principal = parseFloat(document.getElementById("principal").value);
+  var interestRate = parseFloat(document.getElementById("interestRate").value);
+  var years = parseFloat(document.getElementById("years").value);
+
+  var monthlyInterestRate = interestRate / 12 / 100;
+  var months = years * 12;
+
+  var monthlyPayment =
+    (principal * monthlyInterestRate) /
+    (1 - Math.pow(1 + monthlyInterestRate, -months));
+
+  var table = document.createElement('table'); // Create the table element
+
+  // Create the table headers
+  var headerRow = document.createElement('tr');
+  var headers = ['Year', 'Interest', 'Principal', 'Remaining Balance'];
+  headers.forEach(function(headerText) {
+    var headerCell = document.createElement('th');
+    headerCell.textContent = headerText;
+    headerRow.appendChild(headerCell);
+  });
+  table.appendChild(headerRow);
+
+  // Generate the amortization schedule rows
+  var amortizationSchedule = '';
+  for (let i = 0; i < months; i++) {
+    let interest = principal * monthlyInterestRate;
+    let principalPayment = monthlyPayment - interest;
+    let remainingBalance = principal - principalPayment;
+
+    var row = document.createElement('tr');
+    var yearCell = document.createElement('td');
+    yearCell.textContent = Math.floor((i + 1) / 12) + 1;
+    var interestCell = document.createElement('td');
+    interestCell.textContent = interest.toFixed(2);
+    var principalCell = document.createElement('td');
+    principalCell.textContent = principalPayment.toFixed(2);
+    var balanceCell = document.createElement('td');
+    balanceCell.textContent = remainingBalance.toFixed(2);
+
+    row.appendChild(yearCell);
+    row.appendChild(interestCell);
+    row.appendChild(principalCell);
+    row.appendChild(balanceCell);
+
+    table.appendChild(row);
+
+    principal = remainingBalance;
+  }
+
+  var amortizationTableContainer = document.getElementById('amortizationSchedule');
+  amortizationTableContainer.innerHTML = ''; // Clear any existing content
+  amortizationTableContainer.appendChild(table); // Append the table to the container
+}
